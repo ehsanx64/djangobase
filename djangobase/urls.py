3d13bcb6d17de9d2f3e1febb0e3157584b37ae52
Ehsan Mohammadi <ehsanx64@gmail.com>
2020-06-03 17:52:00 +0430

    Added blog app

--- djangobase/urls.py
+++ djangobase/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import site
 
@@ -22,6 +22,7 @@ urlpatterns = [
     # Comment following two lines to get Django's default web page
     path('', site.index),
     path('hello', site.hello),
-    
+
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
